f8d9b13ecf5e7ab496b82a3d1a7a7ec8ccf406b8
Finley Mwangola <finley.mwachia12@gmail.com>
2025-05-01 19:35:12 +0300

    ignore errors during build

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+   typescript: {  
+    ignoreBuildErrors: true, 
+   },
+   eslint : {
+    ignoreDuringBuilds: true,
+   },// Ignore TypeScript errors during build 
+};
 
 export default config;
