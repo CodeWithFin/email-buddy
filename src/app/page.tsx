9b21bcd2f0ee4f2e3bac2de86a0c8e98219e280f
Finley Mwangola <finley.mwachia12@gmail.com>
2025-05-04 08:59:06 +0300

    done with first phase of aurinko setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
+import LinkAccountButton from "@/components/ui/link-account-button";
 export default async function Home() {
-  return <Button>Hello world</Button>;
-    
+  return <LinkAccountButton/>
 }
